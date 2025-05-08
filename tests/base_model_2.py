826b99fa191c43f329892d04e62912ea7915f38f
isamuella <s.ineza@alustudent.com>
2025-05-08 07:55:44 -0700

    ex2

--- tests/base_model_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-bm = BaseModel()
-print(type(bm.id) is str)
-print(len(bm.id) > 0)
-
--- tests/base_model_4.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-bm = BaseModel()
-bm.name = "Test"
-bm.number = 99
-print(bm)
-
