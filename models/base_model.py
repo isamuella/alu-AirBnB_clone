138ff551e4e41b77f0eced8466b3ea9d5687c898
isamuella <s.ineza@alustudent.com>
2025-05-09 00:38:06 -0700

    ex3

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes new instances of BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items()
+            if key == "__class__":
+                continue
+            elif key in ("created_at","updated_at"):
+                setattr(self, key, datetime.fromisoformat(value))
+            else:
+                setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string: [<class name>] (<id>) <__dict__>"""
