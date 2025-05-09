d33d39425f7a6f0feb61e5c67f432f2769e52bb2
isamuella <s.ineza@alustudent.com>
2025-05-09 02:27:18 -0700

    ex5

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """BaseModel that defines all common attribute/methods."""
 import uuid
 from datetime import datetime
-from models import storage
 
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
@@ -22,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage
             storage.new(self)
 
     def __str__(self):
@@ -31,6 +31,7 @@ class BaseModel:
     def save(self):
         """Update updated_at to the current time"""
         self.updated_at = datetime.now()
+        from models import storage
         storage.save()
 
     def to_dict(self):
