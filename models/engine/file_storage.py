cd58100d98a554cbf37a9fd27f372e2656ed8145
isamuella <s.ineza@alustudent.com>
2025-05-13 06:58:12 -0700

    ex5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,11 +5,11 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Returns the dictionary __objects"""
         if cls is None:
             return self.__objects
         else:
@@ -42,27 +42,10 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+        with open(FileStorage.__file_path, 'r') as f:
+            temp = json.load(f)
+            for key, val in temp.items():
+                self.all()[key] = classes[val['__class__']](**val)
 
     def close(self):
         """Deserialize the JSON file to objects"""
