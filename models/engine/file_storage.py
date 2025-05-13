3b1f4c04d2b0756e7a6986c5e24290cded95d8fa
isamuella <s.ineza@alustudent.com>
2025-05-13 07:06:51 -0700

    ex5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import os
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -21,12 +23,12 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all().update({obj.__class__.__name__ + '.' + obj.id: obj})
 
     def delete(self, obj=None):
         """Deletes obj from objects"""
         if obj is not None:
-            key = key = obj.__class__.__name__ + "." + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del self.__objects[key]
                 self.save()
@@ -35,19 +37,28 @@ class FileStorage:
         """Saves storage dictionary to file"""
         with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        with open(FileStorage.__file_path, 'r') as f:
-            temp = json.load(f)
-            for key, val in temp.items():
-                self.all()[key] = classes[val['__class__']](**val)
+        from models.base_model import BaseModel
+        
+        classes = {
+            'BaseModel': BaseModel
+        }
+        
+        try:
+            if os.path.isfile(self.__file_path):
+                with open(self.__file_path, 'r') as f:
+                    temp = json.load(f)
+                    for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
+        except Exception:
+            pass
 
     def close(self):
         """Deserialize the JSON file to objects"""
         self.reload()
-
