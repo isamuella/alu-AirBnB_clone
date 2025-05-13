eb3f28b5d40301bd61561c697a3a5ad6aa9a06b1
isamuella <s.ineza@alustudent.com>
2025-05-13 07:31:43 -0700

    ex6

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A program called console.py that contains the entry point of the command interpreter:
+
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Class that implements a command-line interface for the AirBnB clone application"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """command to exit the program after a new line"""
+        print()
+        return True
+
+    def empty_line(self):
+        """an empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
