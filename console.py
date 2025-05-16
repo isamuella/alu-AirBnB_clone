ea3d81f47e29d8173cf3cf5ba7b560913ad98b49
isamuella <s.ineza@alustudent.com>
2025-05-16 05:47:44 -0700

    ex7

--- console.py
+++ console.py
@@ -1,95 +1,100 @@
+#!/usr/bin/python3
+"""
+A program called console.py that contains the entry point of the command interpreter:
+
+"""
 import cmd
-import json
-import uuid
-from datetime import datetime
-
-class BaseModel:
-    """A simple BaseModel class."""
-    
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def __str__(self):
-        return f"[BaseModel] ({self.id}) {self.__dict__}"
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for HBNB project."""
-    
-    prompt = '(hbnb) '
-    storage = {}
+    """Class that implements a command-line interface for the AirBnB clone application"""
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """command to exit the program after a new line"""
+        print()
+        return True
+
+    def emptyline(self):
+        """an empty line"""
+        pass
+
+    def help_quit(self):
+        """help for the quit command"""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """helf for the EOF command"""
+        print("Exit the program with EOF")
 
     def do_create(self, class_name):
-        """Creates a new instance of BaseModel, saves it, and prints the id."""
+        """Creates a new instance, saves to json, and prints the id"""
         if not class_name:
             print("** class name missing **")
             return
-        if class_name != 'BaseModel':
+        if class_name != "BaseModel":
             print("** class doesn't exist **")
             return
-        
-        new_instance = BaseModel()
-        self.storage[new_instance.id] = new_instance
-        self.save_to_file()
+
+        new_instance = storage.new(class_name)
+        storage.save()
         print(new_instance.id)
 
     def do_show(self, args):
-        """Prints the string representation of an instance based on the class name and id."""
+        """prints the string representation of an instance based on the classname and id"""
         args = args.split()
         if not args:
             print("** class name missing **")
             return
         class_name = args[0]
-        if class_name != 'BaseModel':
-            print("** class doesn't exist **")
+        if class_name != "BaseModel":
+            print("**class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
-        
+
         instance_id = args[1]
-        instance = self.storage.get(instance_id)
+        instance = storage.get(class_name, instance_id)
         if instance is None:
             print("** no instance found **")
         else:
             print(instance)
 
     def do_destroy(self, args):
-        """Deletes an instance based on the class name and id."""
+        """Deletes an instance based on the class name and id"""
         args = args.split()
         if not args:
             print("** class name missing **")
             return
         class_name = args[0]
-        if class_name != 'BaseModel':
+        if class_name != "BaseModel":
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
-        
+
         instance_id = args[1]
-        if instance_id in self.storage:
-            del self.storage[instance_id]
-            self.save_to_file()
+        if storage.delete(class_name, instance_id):
+            storage.save()
         else:
             print("** no instance found **")
 
-    def do_all(self, class_name):
-        """Prints all string representation of all instances based or not on the class name."""
-        if class_name and class_name != 'BaseModel':
+    def do_all(self, args):
+        """Prints all string representation of all instances based or not on the class name"""
+        if class_name and class_name != "BaseModel":
             print("** class doesn't exist **")
             return
-        
-        instances = [str(instance) for instance in self.storage.values()]
-        print(instances)
+        instances = storage.all(class_name)
+        print([str(instance) for instance in instances])
 
     def do_update(self, args):
-        """Updates an instance based on the class name and id by adding or updating attribute."""
+        """Updates an instance based on the class name and id by adding or updating attribute"""
         args = args.split()
         if not args:
             print("** class name missing **")
@@ -103,7 +108,7 @@ class HBNBCommand(cmd.Cmd):
             return
         
         instance_id = args[1]
-        instance = self.storage.get(instance_id)
+        instance = storage.get(class_name, instance_id)
         if instance is None:
             print("** no instance found **")
             return
@@ -119,25 +124,7 @@ class HBNBCommand(cmd.Cmd):
         
         attribute_value = args[3].strip('"')
         setattr(instance, attribute_name, attribute_value)
-        instance.updated_at = datetime.now()
-        self.save_to_file()
-
-    def save_to_file(self):
-        """Saves instances to a JSON file."""
-        with open('file.json', 'w') as f:
-            json.dump({k: v.__dict__ for k, v in self.storage.items()}, f)
-
-    def emptyline(self):
-        """Override emptyline method to do nothing on empty input."""
-        pass
-
-    def do_quit(self, arg):
-        """Quit command to exit the program."""
-        return True
-
-    def do_EOF(self, arg):
-        """Exit the program with EOF."""
-        return True
+        instance.save()  # Save the updated instance
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
