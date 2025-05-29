275abe5b69de4c6a06df9ce4d237fff148d5e19d
isamuella <s.ineza@alustudent.com>
2025-05-29 11:56:02 +0000

    ex0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
-ets up the web servers for the deployment of web_static
+#!/usr/bin/env bash
+# sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
