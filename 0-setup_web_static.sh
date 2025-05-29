c3cf535d66da167a29d5f25b28ed441c5f18f987
isamuella <s.ineza@alustudent.com>
2025-05-29 11:53:52 +0000

    ex0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+ets up the web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo <html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+ </html> | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx start
